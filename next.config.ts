772c236ee82c99d59f373af23f92198e27854718
Xudoberdi Abdujalilov <xudoyberdi0410@gmail.com>
2025-06-26 18:45:35 +0500

    chore: configure deployment to GitHub Pages and update project settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) =>
