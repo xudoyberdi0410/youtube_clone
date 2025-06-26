a5453b574dc97b950ba942a4896ae130ed071c95
Xudoberdi Abdujalilov <xudoyberdi0410@gmail.com>
2025-06-26 21:36:08 +0500

    feat: add generateStaticParams function for dynamic routing in auth page

--- src/app/auth/[mode]/page.tsx
+++ src/app/auth/[mode]/page.tsx
@@ -1,6 +1,13 @@
 import { Auth as AuthComponent } from "@/modules/auth/ui/components/auth";
 import { notFound } from "next/navigation";
 
+export async function generateStaticParams() {
+  return [
+    { mode: 'signin' },
+    { mode: 'signup' }
+  ];
+}
+
 export default async function AuthPage({ params }: {
   params: Promise<{ mode: string }>
 }) {
